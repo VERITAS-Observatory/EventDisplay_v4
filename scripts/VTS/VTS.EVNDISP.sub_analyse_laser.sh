37b754eba4b01b10843938cd2faf2c8bfdb0f4a1
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2012-07-02 06:56:13 +0000

    check that laser run is on disk, otherwise donnot submit
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@835 766874ec-0625-4018-90f7-740f284a29e6

--- scripts/VTS/VTS.EVNDISP.sub_analyse_laser.sh
+++ scripts/VTS/VTS.EVNDISP.sub_analyse_laser.sh
@@ -10,9 +10,18 @@
 
 if [ ! -n "$1" ]
 then
+   echo
+   echo "Laser analysis: submit jobs from a simple run list"
+   echo 
    echo "VTS.EVNDISP.sub_analyse_laser.sh <runlist> "
    echo
    echo "runlist should contain laser run numbers"
+   echo
+   echo "example for run list:"
+   echo "48626"
+   echo "58453"
+   echo "61429"
+   echo
    exit
 fi
 
@@ -28,7 +37,8 @@ fi
 ###############################################################################################################
 
 FILES=`cat $RLIST`
-echo $FILES
+echo "Laser files to analyse:"
+echo "$FILES"
 
 #########################################
 # output directory for error/output from batch system
@@ -48,17 +58,25 @@ FSCRIPT="VTS.EVNDISP.qsub_analyse_laser"
 # loop over all files in files loop
 for AFIL in $FILES
 do
-   echo "now running $AFIL"
-   FNAM="$SHELLDIR/EVN.laser-$AFIL"
+# check if laser file exists
 
-   sed -e "s/RRRRR/$AFIL/" $FSCRIPT.sh > $FNAM.sh
+   DFIL=`find -L $VERITAS_DATA_DIR/data/ -name "$AFIL.cvbf"`
+   if [ -z "$DFIL" ]
+   then
+     echo "Error: laser vbf file not found for run $AFIL"
+   else
+      echo "now running $AFIL"
+      FNAM="$SHELLDIR/EVN.laser-$AFIL"
 
-   chmod u+x $FNAM.sh
-   echo $FNAM.sh
+      sed -e "s/RRRRR/$AFIL/" $FSCRIPT.sh > $FNAM.sh
 
-   qsub -V  -l os="sl*" -l h_cpu=11:29:00 -l h_vmem=2000M -l tmpdir_size=5G -o $QLOG/ -e $QLOG/ "$FNAM.sh"
+      chmod u+x $FNAM.sh
+      echo $FNAM.sh
 
-   sleep 5s
+      qsub -V  -l os="sl*" -l h_cpu=11:29:00 -l h_vmem=2000M -l tmpdir_size=5G -o $QLOG/ -e $QLOG/ "$FNAM.sh"
+
+      sleep 1s
+   fi
 done
 
 exit
