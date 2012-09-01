ab9ad49153da3604ec5b46ab0f347e7e288d9029
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2012-09-01 19:05:21 +0000

    increased showerpars tree version to handle new variables
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@911 766874ec-0625-4018-90f7-740f284a29e6

--- src/VGlobalRunParameter.cpp
+++ src/VGlobalRunParameter.cpp
@@ -260,7 +260,7 @@ void VGlobalRunParameter::printGlobalRunParameter()
 
 string VGlobalRunParameter::fObservatory = "Whipple";
 bool VGlobalRunParameter::bReadRunParameter = false;
-unsigned int VGlobalRunParameter::fEVNDISP_TREE_VERSION = 8;
+unsigned int VGlobalRunParameter::fEVNDISP_TREE_VERSION = 9;
 string VGlobalRunParameter::fEVNDISP_VERSION = "v.4.00";
 string VGlobalRunParameter::fEVNDISP_SVNREVISION = "$Revision$";
 string VGlobalRunParameter::fDBServer = "";
