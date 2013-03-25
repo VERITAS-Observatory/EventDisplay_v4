503ddf29f9396ae658e6e8747cd9323bbeea7bf1
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2013-03-25 09:13:34 +0000

    reduced maximum number of telescopes per analysis to 100 to fit into available memory resources
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@1250 766874ec-0625-4018-90f7-740f284a29e6

--- inc/VGlobalRunParameter.h
+++ inc/VGlobalRunParameter.h
@@ -16,7 +16,7 @@
 #include <string>
 
 // HARDWIRED MAXIMUM NUMBER OF TELESCOPES AND CHANNELS, etc. 
-#define VDST_MAXTELESCOPES  200    // maximum number of telescopes
+#define VDST_MAXTELESCOPES  100    // maximum number of telescopes
 #define VDST_MAXCHANNELS   2900    // maximum number of channels per telescopes
 #define VDST_MAXSUMWINDOW   500    // maximum number of summation windows (=maximum number of samples per FADC trace)
 #define VDST_PEDTIMESLICES 5000    // maximum number of time slices for pedestal calculation
