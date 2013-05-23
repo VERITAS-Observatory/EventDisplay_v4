7c3026491f3d366ea1171c984c4e997dc58414f4
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2013-05-23 09:02:59 +0000

    added offset to WP Phys lot file
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@1358 766874ec-0625-4018-90f7-740f284a29e6

--- scripts/CTA/CTA.WPPhysWriter.qsub.sh
+++ scripts/CTA/CTA.WPPhysWriter.qsub.sh
@@ -17,10 +17,10 @@ set RECID=RRRR
 # set the right observatory (environmental variables)
 source $EVNDISPSYS/setObservatory.tcsh CTA
 
-rm -f $OXUTNAME-$OXBSTIME"h."$AXRRAY.log
+rm -f $OXUTNAME-$OXBSTIME$AXRRAY-$OXFFSET.log
 
 cd $EVNDISPSYS/
-./bin/writeCTAWPPhysSensitivityFiles $AXRRAY $OXBSTIME $DXDIR $OXUTNAME CTA $OXFFSET $RECID > $OXUTNAME-$OXBSTIME$AXRRAY.log
+./bin/writeCTAWPPhysSensitivityFiles $AXRRAY $OXBSTIME $DXDIR $OXUTNAME CTA $OXFFSET $RECID > $OXUTNAME-$OXBSTIME$AXRRAY-$OXFFSET.log
 
 ############################################################################
 
