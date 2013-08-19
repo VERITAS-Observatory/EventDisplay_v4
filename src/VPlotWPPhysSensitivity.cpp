58566d4fc27b3ffad368d82c2b8324ad5ccb347a
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2013-08-19 07:41:28 +0000

    minor adjustments of plotting axis
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@1599 766874ec-0625-4018-90f7-740f284a29e6

--- src/VPlotWPPhysSensitivity.cpp
+++ src/VPlotWPPhysSensitivity.cpp
@@ -479,8 +479,8 @@ bool VPlotWPPhysSensitivity::plotSensitivity( string iPrint, double iMinSensitiv
       else if( iUnit == "CU" ) a->setFluxRange_CU( iMinSensitivity, iMaxSensitivity );
       TCanvas *c_temp = a->plotDifferentialSensitivityvsEnergyFromCrabSpectrum( cSens, "CTA-PHYS", fData[i]->fPlottingColor, iUnit, 0.2, 0.01 );
       if( c_temp ) cSens = c_temp;
-      if( i == 0 ) c_temp = a->plotSignalBackgroundRates( cBck, true, 2.e-7, 1. );   // plot also protons and electrons
-      else         c_temp = a->plotSignalBackgroundRates( cBck, false, 2.e-7, 1. );
+      if( i == 0 ) c_temp = a->plotSignalBackgroundRates( cBck, true, 2.e-7, 14. );   // plot also protons and electrons
+      else         c_temp = a->plotSignalBackgroundRates( cBck, false, 2.e-7, 14. );
       if( c_temp ) cBck = c_temp;
       fillProjectedSensitivityPlot( i, a );
       fData[i]->gSensitivity = (TGraphAsymmErrors*)a->getSensitivityGraph();
