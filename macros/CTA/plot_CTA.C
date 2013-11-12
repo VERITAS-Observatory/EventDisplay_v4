0d87d358473462bd709b994728a782228a4aeefa
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2013-11-12 14:34:47 +0000

    added reading of telescope files; added cost estimates
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@1771 766874ec-0625-4018-90f7-740f284a29e6

--- macros/CTA/plot_CTA.C
+++ macros/CTA/plot_CTA.C
@@ -3,54 +3,271 @@
 
 */
 
-
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TTree.h"
+#include "TText.h"
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
-vector< string > getListofArrrays( string iArrayFile )
+class VPlotCTAArrayLayout_TelescopeList
 {
-   vector< string > SubArray;
-   ifstream is;
-   is.open( iArrayFile.c_str(), ifstream::in );
-   if( !is ) return;
+   public:
 
+   int       fTelID;
+   ULong64_t fTelType;
+   string    fTelTypeName;
+   int       fTelID_hyperArray;
+   float     fTel_x;
+   float     fTel_y;
+   int       fMarkerColor;
+   float     fMarkerSize;
+   int       fMarkerType;
+
+   VPlotCTAArrayLayout_TelescopeList();
+  ~VPlotCTAArrayLayout_TelescopeList() {}
+};
+
+
+class VPlotCTAArrayLayout
+{
+    private:
+
+    vector< VPlotCTAArrayLayout_TelescopeList* > fTelescopeList;
+    vector< VPlotCTAArrayLayout_TelescopeList* > fTelescopeList_subArray;
+    
+    void             drawTelescope( VPlotCTAArrayLayout_TelescopeList*, string iText );
+    vector< string > getListofArrrays( string iArrayFile );
+
+    public:
+    VPlotCTAArrayLayout();
+   ~VPlotCTAArrayLayout() {}
+
+    TCanvas* plot_array( string iArrayName = "", double xmax = 1450., double ymax = 1450., bool drawTelescopeNumbers = true );
+    bool     readArrayFromRootFile( string iFile );
+    void     printArrayCosts();
+    bool     setSubArray( string iSubArrayFile = "" );
+};
+
+VPlotCTAArrayLayout::VPlotCTAArrayLayout()
+{
+}
+
+void VPlotCTAArrayLayout::printArrayCosts()
+{
+   unsigned int iLST = 0;
+   unsigned int iMST = 0;
+   unsigned int iDCSST = 0;
+   unsigned int iSCSST = 0;
+
+   if( fTelescopeList_subArray.size() == 0 ) fTelescopeList_subArray = fTelescopeList;
+
+   for( unsigned int i = 0; i < fTelescopeList_subArray.size(); i++ )
+   {
+      if( fTelescopeList_subArray[i]->fTelTypeName.find( "LST" ) != string::npos )
+      {
+         iLST++;
+      }
+      else if( fTelescopeList_subArray[i]->fTelTypeName.find( "MST" ) != string::npos )
+      {
+         iMST++;
+      }
+      else if( fTelescopeList_subArray[i]->fTelTypeName.find( "DC-SST" ) != string::npos )
+      {
+         iDCSST++;
+      }
+      else if( fTelescopeList_subArray[i]->fTelTypeName.find( "SC-SST" ) != string::npos )
+      {
+         iSCSST++;
+      }
+   }
+
+// in MEuro
+   float euro_LST = 6.3;
+   float euro_MST = 1.725;
+   float euro_SST = 0.50;
+
+   cout << endl;
+   cout << "# of LSTs: " << iLST << " (" << iLST * euro_LST << ")" <<  endl;
+   cout << "# of MSTs: " << iMST << " (" << iMST * euro_MST << ")" << endl; 
+   cout << "# of DCSSTs: " << iDCSST << " (" << iDCSST * euro_SST << ")" << endl;
+   cout << "# of SCSSTs: " << iSCSST << " (" << iSCSST * euro_SST << ")" << endl;
+   cout << "===============================================" << endl;
+   cout << "Tot: " << iLST * euro_LST + iMST * euro_MST + iDCSST * euro_SST + iSCSST * euro_SST << endl;
+   cout << endl;
+ 
+}
+   
+
+bool VPlotCTAArrayLayout::setSubArray( string iSubArrayFile )
+{
+    fTelescopeList_subArray.clear();
+    if( iSubArrayFile.size() == 0 )
+    {
+        return true;
+    }
+// read list of telescope IDs
+   ifstream is;
+   is.open( iSubArrayFile.c_str(), ifstream::in );
+   if( !is )
+   {
+     cout << "error, subarray file not found: " << endl;
+     cout << iSubArrayFile << endl;
+     return false;
+   }
+
+   int iTelID = 0;
    string is_line;
    while( getline( is, is_line ) ) 
    {
-      SubArray.push_back( is_line );
-   }
+      if( is_line.size() > 0 )
+      {
+         istringstream is_stream( is_line );
+         is_stream >> iTelID;
+         if( iTelID == 0 ) continue;
 
-   return SubArray;
+         for( unsigned int i = 0; i <  fTelescopeList.size(); i++ )
+         {
+            if( iTelID ==  fTelescopeList[i]->fTelID )
+            {
+               fTelescopeList_subArray.push_back( fTelescopeList[i] );
+            }
+         }
+      }
+   }
+   is.close();
+
+   cout << "Selected sub array with " << fTelescopeList_subArray.size() << " telescopes (from " << fTelescopeList.size() << ")" << endl;
+
+   return true;
 }
+
+
+/*
+ * read list of telescopes from root file
+ *
+*/
+
+bool VPlotCTAArrayLayout::readArrayFromRootFile( string iFile )
+{
+    fTelescopeList.clear();
+    fTelescopeList_subArray.clear();
+
+    TFile *f1 = new TFile( iFile.c_str() );
+    if( f1->IsZombie() ) return false;
+
+    TTree *t = (TTree*)f1->Get( "telconfig");
+    if( !t ) return false;
+
+    cout << "Telconfig tree found with " << t->GetEntries() << " telescopes" << endl;
+
+    float iTelX = 0.;
+    float iTelY = 0.;
+    ULong64_t iTelType = 0;
+    int iTelID = 0;
+    unsigned int iTelIDHA = 0;
+
+    t->SetBranchAddress( "TelID", &iTelID );
+    t->SetBranchAddress( "TelType", &iTelType );
+    if( t->GetBranchStatus( "TelID_hyperArray" ) ) t->SetBranchAddress( "TelID_hyperArray", &iTelIDHA );
+    t->SetBranchAddress( "TelX", &iTelX );
+    t->SetBranchAddress( "TelY", &iTelY );
+
+    for( int i = 0; i < t->GetEntries(); i++ )
+    {
+       t->GetEntry( i );
+
+       fTelescopeList.push_back( new VPlotCTAArrayLayout_TelescopeList() );
+
+       fTelescopeList.back()->fTelID = iTelID;
+       fTelescopeList.back()->fTelType = iTelType;
+       fTelescopeList.back()->fTelID_hyperArray = (int)iTelIDHA;
+       fTelescopeList.back()->fTel_x = iTelX;
+       fTelescopeList.back()->fTel_y = iTelY;
+// LSTs (1)
+       if( iTelType == 138704810 || iTelType == 141305009 || iTelType == 141305109 )
+       {
+           fTelescopeList.back()->fTelTypeName = "23m-LST";
+           fTelescopeList.back()->fMarkerColor = 2;
+           fTelescopeList.back()->fMarkerSize = 2;
+           fTelescopeList.back()->fMarkerType = 24;
+       }
+// standard MSTs (2)
+       else if( iTelType == 10007818 || iTelType == 10408418 || iTelType == 10008118 )
+       {
+           fTelescopeList.back()->fTelTypeName = "12m-MST";
+           fTelescopeList.back()->fMarkerColor = 9;
+           fTelescopeList.back()->fMarkerSize = 1.5;
+       }
+// large pixel MSTs (4)
+       else if( iTelType == 10009725 )
+       {
+           fTelescopeList.back()->fTelTypeName = "12m-MST-LPix";
+           fTelescopeList.back()->fMarkerColor = 6;
+           fTelescopeList.back()->fMarkerSize = 1.5;
+       }
+// standard SSTs (3)
+       else if( iTelType == 3709725 || iTelType == 3709425 || iTelType == 3710125 )
+       {
+           fTelescopeList.back()->fTelTypeName = "7m-DC-SST";
+           fTelescopeList.back()->fMarkerColor = 3;
+           fTelescopeList.back()->fMarkerSize = 1;
+       }
+// 7m telescopes (5, prod1) or SCT (prod2)
+       else if( iTelType == 7309930  || iTelType == 201509515 )
+       {
+           fTelescopeList.back()->fTelTypeName = "4m-SC-SST";
+           fTelescopeList.back()->fMarkerColor = 4;
+           fTelescopeList.back()->fMarkerSize = 1;
+       }
+       else
+       {
+          cout << "unknown telescope type: " << iTelType << endl;
+       }
+    }
+
+    return true;
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 
 /*
  
    plot a single telescope
 
 */
-
-void drawTelescope( double x, double y, string iText, double iMarkerMult, int iMarkerColor )
+void VPlotCTAArrayLayout::drawTelescope( VPlotCTAArrayLayout_TelescopeList *iD, string iText )
 {
-    TGraph *iG = new TGraph( 1 );
-    iG->SetMarkerStyle( 20 );
-    iG->SetMarkerSize( 1.5 * iMarkerMult );
-    iG->SetMarkerColor( iMarkerColor );
+    if( !iD ) return;
 
-    iG->SetPoint( 0, x, y );
+    TGraph *iG = new TGraph( 1 );
+    iG->SetMarkerStyle( iD->fMarkerType );
+    iG->SetMarkerSize( iD->fMarkerSize );
+    iG->SetMarkerColor( iD->fMarkerColor );
+
+    iG->SetPoint( 0, iD->fTel_x, iD->fTel_y );
 
     iG->Draw( "p" );
 
     if( iText.size() > 0 )
     {
-       TText *iT = new TText( x, y, iText.c_str() );
+       TText *iT = new TText( iD->fTel_x, iD->fTel_y, iText.c_str() );
        iT->SetTextSize( 0.014 );
+       iT->SetTextColor( 2 );
        iT->Draw();
     }
 }
-
 
 
 /*
@@ -68,17 +285,22 @@ void drawTelescope( double x, double y, string iText, double iMarkerMult, int iM
      plots
 
 */
-TCanvas* plot_array( char *ifile, char *iname = 0, double iMarkerMult = 1., double xmax = 1450., double ymax = 1450., bool drawTelescopeNumbers = true )
+TCanvas* VPlotCTAArrayLayout::plot_array( string iname, double xmax, double ymax, bool drawTelescopeNumbers )
 {
-    TFile *f1 = new TFile( ifile );
-    if( f1->IsZombie() ) return 0;
+    char hname[200];
+    char htitle[200];
+    if( iname.size() > 0 )
+    {
+       sprintf( hname, "cArrayLayout_%s", iname.c_str() );
+       sprintf( htitle, "array layout (%s)", iname.c_str() );
+    }
+    else
+    {
+       sprintf( hname, "cArrayLayout" );
+       sprintf( htitle, "array layout" );
+    }
 
-    TTree *t = (TTree*)f1->Get( "telconfig");
-    if( !t ) return 0;
-
-    cout << "Array " << iname << ": telconfig tree found with " << t->GetEntries() << " telescopes" << endl;
-
-    TCanvas *c = new TCanvas( "c2", "array layout", 10, 10, 600, 600 );
+    TCanvas *c = new TCanvas( hname, htitle, 10, 10, 800, 800 );
     c->SetGridx( 0 );
     c->SetGridy( 0 );
     c->SetRightMargin( 0.05 );
@@ -93,76 +315,32 @@ TCanvas* plot_array( char *ifile, char *iname = 0, double iMarkerMult = 1., doub
 
     hnull->Draw();
 
-    float iTelX = 0.;
-    float iTelY = 0.;
-    ULong64_t iTelType = 0;
-    int iTelID = 0;
-    unsigned int iTelIDHA = 0;
-
-    t->SetBranchAddress( "TelID", &iTelID );
-    t->SetBranchAddress( "TelType", &iTelType );
-    t->SetBranchAddress( "TelID_hyperArray", &iTelIDHA );
-    t->SetBranchAddress( "TelX", &iTelX );
-    t->SetBranchAddress( "TelY", &iTelY );
-
-    int iMarkerColor = 0;
-    char hname[200];
-
-    unsigned int iLST = 0;
-    unsigned int iMST = 0;
-    unsigned int iDCSST = 0;
-    unsigned int iSCSST = 0;
-
-    for( int i = 0; i < t->GetEntries(); i++ )
+// check that sub array is set
+    if( fTelescopeList_subArray.size() == 0 )
     {
-       t->GetEntry( i );
+        fTelescopeList_subArray = fTelescopeList;
+    }
 
-// LSTs (1)
-       if( iTelType == 138704810 || iTelType == 141305009 || iTelType == 141305109 )
+    for( unsigned int i = 0; i < fTelescopeList_subArray.size(); i++ )
+    {
+       if( !fTelescopeList_subArray[i] ) continue;
+
+       if( drawTelescopeNumbers )
        {
-          iMarkerColor = 2;
-	  iLST++;
-       }
-// standard MSTs (2)
-       else if( iTelType == 10007818 || iTelType == 10408418 || iTelType == 10008118 )
-       {
-          iMarkerColor = 1;
-	  iMST++;
-       }
-// large pixel MSTs (4)
-       else if( iTelType == 10009725 )  iMarkerColor = 6;
-// standard SSTs (3)
-       else if( iTelType == 3709725 || iTelType == 3709425 || iTelType == 3710125 )
-       {
-          iMarkerColor = 3;
-	  iDCSST++;
-       }
-// 7m telescopes (5, prod1) or SCT (prod2)
-       else if( iTelType == 7309930  || iTelType == 201509515 )
-       {
-          iMarkerColor = 4;
-	  iSCSST++;
+          sprintf( hname, "%d, %d", fTelescopeList_subArray[i]->fTelID, fTelescopeList_subArray[i]->fTelID_hyperArray );
+          drawTelescope( fTelescopeList_subArray[i], hname );
        }
        else
        {
-          cout << "unknown telescope type: " << iTelType << endl;
-	  continue;
+          drawTelescope( fTelescopeList_subArray[i], "" );
        }
-
-       if( drawTelescopeNumbers ) sprintf( hname, "%d, %d", iTelID, iTelIDHA );
-       else                       sprintf( hname, "" );
-       drawTelescope( iTelX, iTelY, hname, iMarkerMult, iMarkerColor );
-
+          
 
     }
-    cout << "# of LSTs: " << iLST << endl;
-    cout << "# of MSTs: " << iMST << endl;
-    cout << "# of DCSSTs: " << iDCSST << endl;
-    cout << "# of SCSSTs: " << iSCSST << endl;
 // draw array name
-    if( iname )
+    if( iname.size() > 0 )
     {
-       TText *it = new TText( -1.*0.9*xmax, 0.75*ymax, iname );
+       TText *it = new TText( -1.*0.9*xmax, 0.75*ymax, iname.c_str() );
        it->SetTextSize( it->GetTextSize() * 1.5 );
        it->Draw();
     }
@@ -170,29 +348,34 @@ TCanvas* plot_array( char *ifile, char *iname = 0, double iMarkerMult = 1., doub
     return c;
 }
 
-/*
-
-    plot all array layout from a list of runs
-
-    (read telconfig trees from evndisp output files)
-
-*/
-void plot_allArrayLayouts( string iArrayListFile, string iFileName, double iMarkerMult = 1., double xmax = 1450., double ymax = 1450., string iDataSet = "prod2-Aar-North" )
+vector< string > VPlotCTAArrayLayout::getListofArrrays( string iArrayFile )
 {
-   vector< string > iVArray = getListofArrrays( iArrayListFile );
+   vector< string > SubArray;
+   ifstream is;
+   is.open( iArrayFile.c_str(), ifstream::in );
+   if( !is ) return SubArray;
 
-   char hname[2000];
-
-   for( unsigned int i = 0; i < iVArray.size(); i++ )
+   string is_line;
+   while( getline( is, is_line ) ) 
    {
-      sprintf( hname, "$CTA_USER_DATA_DIR/analysis/AnalysisData/%s/%s/gamma_onSource/%s", iDataSet.c_str(), iVArray[i].c_str(), iFileName.c_str() );
-      cout << hname << endl;
-
-      TCanvas *cC = plot_array( hname, iVArray[i].c_str(), iMarkerMult, xmax, ymax );
-      if( cC )
-      {
-         sprintf( hname, "ArrayLayout-%s.eps", iVArray[i].c_str() );
-	 cC->Print( hname );
-      }
+      SubArray.push_back( is_line );
    }
+
+   return SubArray;
+}
+
+////////////////////////////////////////////////////////////
+
+VPlotCTAArrayLayout_TelescopeList::VPlotCTAArrayLayout_TelescopeList()
+{
+    fTelID = 0;
+    fTelType = 0;
+    fTelID_hyperArray = 0;
+    fTel_x = 0.;
+    fTel_y = 0.;
+    fTelTypeName = "";
+
+    fMarkerColor = 1;
+    fMarkerSize = 1;
+    fMarkerType = 8;
 }
