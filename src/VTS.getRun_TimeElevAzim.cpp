516ec145c253e531e35e13d8dfdf8cd5a8a27ce1
nkelhos <nkelhos@766874ec-0625-4018-90f7-740f284a29e6>
2013-11-19 15:52:45 +0000

    removed hardcoding of database url
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@1806 766874ec-0625-4018-90f7-740f284a29e6

--- src/VTS.getRun_TimeElevAzim.cpp
+++ src/VTS.getRun_TimeElevAzim.cpp
@@ -3,6 +3,8 @@
 //  the telescope is pointing at once per second
 #include "CData.h"
 #include "VSkyCoordinates.h"
+#include "VDB_Connection.h"
+#include "VGlobalRunParameter.h"
 #include <TSQLResult.h>
 #include <TSQLRow.h>
 #include <TSQLServer.h>
@@ -13,21 +15,51 @@ using namespace std;
 int main( int argc, char *argv[] )
 {
 	// parse argument
-	if ( argc != 2 ) { cout << "Needs runnumber, exiting..." << endl; exit(1) ; }
+	if ( argc != 2 ) { 
+		cout << "Error, needs runnumber argument." << endl; 
+		cout << "For Example:" << endl;
+		cout << "  $ VTS.getRun_TimeElevAzim 65765" << endl;
+		exit(1) ;
+	}
 	int runnumber = atoi( argv[1] ) ;
-	if ( runnumber < 9999 or runnumber > 99999 ) { cout << "Needs valid runnumber (9999-99999)" << endl; exit(1); }
+	if ( runnumber < 9999 or runnumber > 99999 ) { 
+		cout << "Error, needs valid runnumber (9999-99999) (read in " << runnumber << ")." << endl;
+		exit(1); 
+	}
 	//printf( "examining run %d...\n", runnumber ) ;
 	
+	VGlobalRunParameter *blah = new VGlobalRunParameter() ;
+	cout <<" VGlobal->getDBServer(): " << blah->getDBServer() << endl;
 	// start connection
-	string db_name = "mysql://romulus.ucsc.edu/" ;
-	TSQLServer * f_db = TSQLServer::Connect( db_name.c_str(), "readonly" , "" );
+	//string db_name = "mysql://romulus.ucsc.edu/" ;
+	//TSQLServer * f_db = TSQLServer::Connect( db_name.c_str(), "readonly" , "" );
+	// fixed below to follow the party line
+	string tmpdb_ver;
+	tmpdb_ver = blah->getDBServer() + "/VERITAS" ;
+	cout << "Using server " << tmpdb_ver << endl;
+	VDB_Connection my_connection_ver( tmpdb_ver.c_str(), "readonly", "" ) ;
+    if(!my_connection_ver.Get_Connection_Status()) {
+        cout << "error connecting to db" << endl;
+        return -1;
+    }
 	
 	// get run start and finish, # of telescopes
-	char query[1000] ;
-	sprintf( query, "select run_id, data_start_time, data_end_time, config_mask from VERITAS.tblRun_Info where run_id = %d", runnumber ) ;
+	char c_query[1000] ;
+	sprintf( c_query, "select run_id, data_start_time, data_end_time, config_mask from tblRun_Info where run_id = %d", runnumber ) ;
 	//printf( "%s\n", query ) ;
-	TSQLResult *db_res = f_db->Query( query );
-    if( !db_res ) { cout << "TSQLResult empty, exiting..." << endl ; exit(1) ; }
+    if(!my_connection_ver.make_query(c_query) ){
+		cout << "Error, unable to get response from db for query \"" << c_query << "\"" << endl;
+		cout << "Exiting..." << endl;
+    	return -1;
+    }
+
+	//TSQLResult *db_res = f_db->Query( query );
+	TSQLResult *db_res = my_connection_ver.Get_QueryResult();
+	
+    if( !db_res ) {
+		cout << "TSQLResult empty, exiting..." << endl ;
+		exit(1) ;
+	}
 	int fNRows = db_res->GetRowCount();
 	string itemp;
 	double MJDStart=0.0, MJDEnd=0.0 ;
@@ -54,6 +86,14 @@ int main( int argc, char *argv[] )
 
 	//printf( "Run %d started at %f and ended at %f, using telescope combo %lu\n", runnumber, MJDStart, MJDEnd, ImgSel ) ;
 	
+	string tmpdb_vof;
+	tmpdb_vof = blah->getDBServer() + "/VOFFLINE" ;
+	VDB_Connection my_connection_vof( tmpdb_ver.c_str(), "readonly", "" ) ;
+    if(!my_connection_vof.Get_Connection_Status()) {
+        cout << "error connecting to db VOFFLINE" << endl;
+        return -1;
+    }
+	
 	// request pointing data
 	vector <double> mjd  ;
 	vector <int>    mjdDay ;
@@ -66,9 +106,16 @@ int main( int argc, char *argv[] )
 	for ( unsigned int i_tel = 0 ; i_tel < 4 ; i_tel++ )
 	{
 		if ( ! a.test( i_tel ) ) continue ;
-		sprintf( query, "select mjd, ra, decl from VOFFLINE.tblPointing_Monitor_Telescope%d_Calibrated_Pointing where mjd > %f and mjd < %f ;", i_tel, MJDStart, MJDEnd ) ;
+		sprintf( c_query, "select mjd, ra, decl from VOFFLINE.tblPointing_Monitor_Telescope%d_Calibrated_Pointing where mjd > %f and mjd < %f ;", i_tel, MJDStart, MJDEnd ) ;
 		//printf( "%s\n", query) ;
-		TSQLResult *db_res2 = f_db->Query( query );
+		if(!my_connection_vof.make_query(c_query) ){
+			cout << "Error, unable to get response from db for query \"" << c_query << "\"" << endl;
+			cout << "Exiting..." << endl;
+			return -1;
+		}
+		
+		//TSQLResult *db_res2 = f_db->Query( query );
+		TSQLResult *db_res2 = my_connection_vof.Get_QueryResult();
 		fNRows = db_res2->GetRowCount();
 		//int count=0 ;
 		//printf( "%d rows\n", fNRows ) ;
@@ -105,7 +152,7 @@ int main( int argc, char *argv[] )
 			break ; 
 		}
 	}
-	f_db->Close() ;
+	//f_db->Close() ;
 	
 	// convert pointing data from ra/dec to elev/azi
 	float lat = 31.675  ; // observatory coordinates
