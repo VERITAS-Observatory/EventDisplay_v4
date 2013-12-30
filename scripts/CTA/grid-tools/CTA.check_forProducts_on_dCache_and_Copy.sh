b15faa0fd45a25420a584f6c3ae2d1e9cb49dceb
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2013-12-30 15:10:46 +0000

    added mcaz option to dCache cp scripts
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@1863 766874ec-0625-4018-90f7-740f284a29e6

--- scripts/CTA/grid-tools/CTA.check_forProducts_on_dCache_and_Copy.sh
+++ scripts/CTA/grid-tools/CTA.check_forProducts_on_dCache_and_Copy.sh
@@ -8,7 +8,7 @@
 if [ $# -lt 5 ]
 then
    echo
-   echo "CTA.check_forProducts_on_dCache_and_Copy.sh <sub array list> <run list> <data directory> <destination dir> <file with missing runs>"
+   echo "CTA.check_forProducts_on_dCache_and_Copy.sh <sub array list> <run list> <data directory> <destination dir> <file with missing runs> [MCaz]"
    echo
    echo " (note: very dependent on file names"
    echo
@@ -43,16 +43,31 @@ do
       else
          ARRAY=`basename $a .lis`
          ARRAY=${ARRAY:11}
-         DDIR=$4/$ARRAY/proton_grid_131217
-         echo $ARRAY
-         echo $DDIR
+#         DDIR=$4/$ARRAY/proton_grid_131217
+         DDIR=$4/$ARRAY/gamma_onSource
          mkdir -p $DDIR
+# check if file already exists
+         if [ -n "$6" ]
+         then
+            VFIL="$DDIR/$RUN"_G_"$MCAZ"deg.root
+            if [ -e $VFIL ]
+            then
+               echo "found $VFIL"
+               continue
+            fi
+         fi
          echo "copying $EFIL"
          dccp $EFIL $DDIR/$RUN"_"$a"_evndisp.root"
-         MCAZ=`$EVNDISPSYS/bin/printRunParameter $DDIR/$RUN"_"$a"_evndisp.root" -mcaz`
+         if [ ! -n "$6" ]
+         then
+            MCAZ=`$EVNDISPSYS/bin/printRunParameter $DDIR/$RUN"_"$a"_evndisp.root" -mcaz`
+         else
+            MCAZ=$6
+         fi
+         VFIL="$DDIR/$RUN"_G_"$MCAZ"deg.root
          mv -v -f $DDIR/$RUN"_"$a"_evndisp.root" $DDIR/$RUN"_G_"$MCAZ"deg.root"
 # soft link into main proton directory
-        ln -f -s $DDIR/$RUN"_G_"$MCAZ"deg.root" $4/$ARRAY/proton/$RUN"_G_"$MCAZ"deg.root"
+#        ln -f -s $DDIR/$RUN"_G_"$MCAZ"deg.root" $4/$ARRAY/proton/$RUN"_G_"$MCAZ"deg.root"
       fi
     done
 done
