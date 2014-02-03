8cb4e02765af2bcfba2760570fc520c7f01835b3
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2014-02-03 16:29:10 +0000

    added flags to make tables with CARE simulations
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@1980 766874ec-0625-4018-90f7-740f284a29e6

--- scripts/VTS/VTS.MSCW_ENERGY.qsub_make_tables.sh
+++ scripts/VTS/VTS.MSCW_ENERGY.qsub_make_tables.sh
@@ -16,17 +16,29 @@ LOGDIR=CCCCCC
 ODDIR=DDDDDD
 IZE=ZENITH
 WOFF=WOBBLEOFFSET
+SIMU=SIMS
 
 source $EVNDISPSYS/setObservatory.sh VERITAS
 
 # this is where the executable can be found
 cd $EVNDISPSYS/bin
 
+#####################################################################
+# GRISUDET simulations
+if [ $SIMU = "GRISU" ]
+then
 # subdirectory with date where evndisp simulation files are found
-DSET="analysis_d20131031"
-
+    DSET="analysis_d20131031"
 # directory with input file
-DDIR="$VERITAS_DATA_DIR/analysis/EVDv400/"$ARRAY"_FLWO/gamma_"$IZE"deg_750m/wobble_"$WOFF"/"$DSET"_ATM"$ATMO"_"$ANAC"_NOISE"$NOISEX"/*.root"
+    DDIR="$VERITAS_DATA_DIR/analysis/EVDv400/"$ARRAY"_FLWO/gamma_"$IZE"deg_750m/wobble_"$WOFF"/"$DSET"_ATM"$ATMO"_"$ANAC"_NOISE"$NOISEX"/*.root"
+else
+#####################################################################
+# CARE simulations
+# subdirectory with date where evndisp simulation files are found
+   DSET="analysisCARE_d20140127"
+# directory with input file
+   DDIR="$VERITAS_DATA_DIR/analysis/EVDv400/"$ARRAY"_FLWO/care_Jan1427/gamma_"$IZE"deg_750m/wobble_"$WOFF"/"$DSET"_ATM"$ATMO"_"$ANAC"_NOISE"$NOISEX"/*.root"
+fi
 
 # remove existing log and table file
 rm -f $ODDIR/$TFIL-NOISE$NOISEY-$IZE-$WOFF.root
--- scripts/VTS/VTS.MSCW_ENERGY.sub_make_tables.sh
+++ scripts/VTS/VTS.MSCW_ENERGY.sub_make_tables.sh
@@ -7,13 +7,16 @@
 #
 #
 
-if [ ! -n "$1" ] && [ ! -n "$2" ] && [ ! -n "$3" ] || [ ! -n "$4" ]
+if [ $# -lt 5 ]
 then
    echo
-   echo "VTS.MSCW_ENERGY.sub_make_tables.sh <table file> <recid> <atmo ID=21/22> <array=V4/V5/V6>"
+   echo "VTS.MSCW_ENERGY.sub_make_tables.sh <table file> <recid> <atmo ID=21/22> <array=V4/V5/V6> <GRISU/CARE>"
    echo
    echo " recid: array rec id according to array_analysis_cuts.txt"
    echo
+   echo " <GRISU/CARE>: simulation package used"
+   echo
+   echo $#
    exit
 fi
 
@@ -22,6 +25,7 @@ ANAC="1234"
 RECID=$2
 ARRAY=$4
 ATMO=$3
+SIM=$5
 
 if [ -e $TFIL ]
 then
@@ -34,27 +38,42 @@ FSCRIPT="VTS.MSCW_ENERGY.qsub_make_tables"
 #################################################################
 # zenith angle bins
 IZE=( 00 20 30 35 40 45 50 55 60 65 ) 
+IZE=( 00 20 30 35 )
 NZE=${#IZE[@]}
+#################################################################
 # wobble off bins
 WOFF=( 0.00 0.25 0.5 0.75 1.00 1.25 1.50 1.75 2.00 )
+WOFF=( 0.5 )
 NWOF=${#WOFF[@]}
+#################################################################
 # noise levels
-INOI=( 075 100 150 200 250 325  425  550  750 1000 )
+##################
+# GRISU  Simulations
+if [ $SIM = "GRISU" ]
+then
+    INOI=( 075 100 150 200 250 325  425  550  750 1000 )
 # mean ped var
 # (short summation window)
 # determined from mean pedvar of simulations
 # (cannot be determined on the fly due to floating point uncertainty)
 # from sumwindow=6
-if [ $ARRAY = "V5" ] || [ $ARRAY = "V4" ]
-then
-   TNOI=( 336 382 457 524 579 659 749 850 986 1138 )
-else
-   TNOI=( 331 384 469 542 600 684 773 875 1002 1178 )
-fi
+    if [ $ARRAY = "V5" ] || [ $ARRAY = "V4" ]
+    then
+       TNOI=( 336 382 457 524 579 659 749 850 986 1138 )
+    else
+       TNOI=( 331 384 469 542 600 684 773 875 1002 1178 )
+    fi
 # from sumwindow=7
 # TNOI=( 375 430 515 585 650 740 840 950 1105 1280 )
 # from sumwindow=12
 # TNOI=( 535 610 730 840 935 1060 1210 1370 1595 1840 )
+##################
+# CARE Simulations
+else
+# from sumwindow=6
+    INOI=(  50  80 120 170 230 290  370  450 )
+    TNOI=( 349 410 607 683 800 888 1001 1108 )
+fi
 NNOI=${#INOI[@]}
 #################################################################
 
@@ -112,12 +131,13 @@ do
 	     -e "s|CCCCCC|$LOGDIR|" \
 	     -e "s|DDDDDD|$ODDIR|" \
 	     -e "s|ZENITH|${IZE[$N]}|" \
+             -e "s|SIMS|$SIM|" \
 	     -e "s|WOBBLEOFFSET|${WOFF[$W]}|" $FSCRIPT.sh > $FNAM.sh
 
 	 chmod u+x $FNAM.sh
 
 # submit job
-	qsub -V -l os=sl6 -l h_cpu=3:29:00 -l h_vmem=8000M -l tmpdir_size=1G -o $QLOG/ -e $QLOG/ "$FNAM.sh"
+#	qsub -V -l os=sl6 -l h_cpu=3:29:00 -l h_vmem=8000M -l tmpdir_size=1G -o $QLOG/ -e $QLOG/ "$FNAM.sh"
 
         let "W = $W + 1"
      done
