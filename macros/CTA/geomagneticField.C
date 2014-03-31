d304102eeb64ee41601cf05e6677cfdb747165a1
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2014-03-31 10:42:55 +0000

    small macro to print the relevant components of the geomagnetic field at the CTA candidate sites
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@2187 766874ec-0625-4018-90f7-740f284a29e6

---
+++ macros/CTA/geomagneticField.C
@@ -0,0 +1,87 @@
+/*
+         calculate the geomagnetic field compontents at the CTA site candidates
+*/
+
+double orthogonal( double zenith_deg, double azimuth_deg, double B_z, double B_x )
+{
+
+    double x = -sin( zenith_deg * TMath::DegToRad() ) * sin( azimuth_deg * TMath::DegToRad() ) * B_z;
+    double y =  sin( zenith_deg * TMath::DegToRad() ) * cos( azimuth_deg * TMath::DegToRad() ) * B_z
+	       - cos( zenith_deg * TMath::DegToRad() ) * B_x;  
+    double z = sin( zenith_deg * TMath::DegToRad() ) * sin( zenith_deg * TMath::DegToRad() ) * B_x;
+
+    return sqrt( x*x + y*y + z*z );
+}
+
+/*
+     all values from the prod2 CORSIKA simulation header
+
+*/
+void printBField_at_CTA_sites()
+{
+   vector< double > fZe;
+   vector< double > fAz;
+   fZe.push_back( 20. );
+   fZe.push_back( 40. );
+   fAz.push_back( 0. );
+   fAz.push_back( 180. );
+
+   vector< string > fSiteName;
+   vector< double > fB_tot;
+   vector< double > fInclination_deg;
+   vector< double > fDeclination_deg;
+
+   fSiteName.push_back( "Leoncito" );
+   fB_tot.push_back( 23.5536 );
+   fInclination_deg.push_back( -32.3406 );
+   fDeclination_deg.push_back( 0. );
+   
+   fSiteName.push_back( "SAC" );
+   fB_tot.push_back( 22.7161  );
+   fInclination_deg.push_back( -23.0661 );
+   fDeclination_deg.push_back( 0. );
+
+   fSiteName.push_back( "Aar" );
+   fB_tot.push_back( 27.1812 );
+   fInclination_deg.push_back( -66.3586 );
+   fDeclination_deg.push_back( 0. );
+
+   fSiteName.push_back( "Tenerife" );
+   fB_tot.push_back( 38.5314 );
+   fInclination_deg.push_back( 36.952 );
+   fDeclination_deg.push_back( 0. );
+
+   fSiteName.push_back( "SPM" );
+   fB_tot.push_back( 45.8194 );
+   fInclination_deg.push_back( 56.6131 );
+   fDeclination_deg.push_back( 0. );
+
+   fSiteName.push_back( "US" );
+   fB_tot.push_back( 48.9437 );
+   fInclination_deg.push_back( 61.2665 );
+   fDeclination_deg.push_back( 0. );
+
+   for( unsigned int i = 0; i < fSiteName.size(); i++ )
+   {
+      cout << "=========================" << endl;
+      cout << fSiteName[i] << endl;
+      for( unsigned int j = 0; j < fZe.size(); j++ )
+      {
+	 for( unsigned int k = 0; k < fAz.size(); k++ )
+	 {
+              cout << "\t Ze=" << fZe[j] << " deg, Az=" << fAz[k] << " deg: ";
+	      cout << " B_tot = " << fB_tot[i] << ", inclination " << fInclination_deg[i];
+	      cout << " deg, declination " << fDeclination_deg[i] << " deg, ";
+	      double B_h = fB_tot[i] * cos( fInclination_deg[i] * TMath::DegToRad() );
+	      double B_z = fB_tot[i] * sin( fInclination_deg[i] * TMath::DegToRad() );
+	      double B_x = B_h * cos( fDeclination_deg[i] * TMath::DegToRad() );
+	      cout << "B_h = " << B_h;
+	      cout << ", B_z = " << B_z;
+	      cout << ", B_x = " << B_x;
+	      cout << ", B_orth = " << orthogonal( fZe[j], fAz[k], B_z, B_x ) << endl;
+	 }
+      }
+   }
+
+}
+
