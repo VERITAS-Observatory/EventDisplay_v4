af29a7c3264bbc70cedd1a8dc13238c36f4ba723
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2014-05-16 15:37:16 +0000

    removed some bugs
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@2343 766874ec-0625-4018-90f7-740f284a29e6

--- scripts/VTS/ANALYSIS.evndisp.sh
+++ scripts/VTS/ANALYSIS.evndisp.sh
@@ -9,22 +9,26 @@ if [ ! -n "$1" ] || [ "$1" = "-h" ]; then
 echo "
 EVNDISP data analysis: submit jobs from a simple run list
 
-ANALYSIS.evndisp.sh <runlist> [output directory] [calibration] [VPM]
+ANALYSIS.evndisp.sh <runlist> <output directory> [calibration] [VPM] [mscw directory]
 
 required parameters:
 
     <runlist>               simple run list with one run number per line
+    
+    <output directory>      directory where output ROOT files will be stored
 
 optional parameters:
     
-    [output directory]      directory where output ROOT files will be stored
-
     [calibration]
           1                 pedestal & average tzero calculation (default)
           2                 pedestal calculation only
           3                 average tzero calculation only
 
     [VPM]                   set to 0 to switch off (default is on)
+    
+    [mscw directory]        directory which contains mscw_energy files
+                            only used by frogs analysis, and 
+                            only if \$USEFROGS=true
 
 --------------------------------------------------------------------------------
 "
@@ -33,44 +37,52 @@ exit
 fi
 
 # Run init script
-bash $(dirname "$0")"/helper_scripts/UTILITY.script_init.sh"
+bash "$( cd "$( dirname "$0" )" && pwd )/helper_scripts/UTILITY.script_init.sh"
 [[ $? != "0" ]] && exit 1
+
+# create extra stdout for duplication of command output
+# look for ">&5" below
+exec 5>&1
 
 # Parse command line arguments
 RLIST=$1
-[[ "$2" ]] && ODIR=$2  || ODIR="$VERITAS_USER_DATA_DIR/analysis/Results/$EDVERSION/"
+ODIR=$2
+mkdir -p $ODIR
 [[ "$3" ]] && CALIB=$3 || CALIB=1
 [[ "$4" ]] && VPM=$4   || VPM=1
 
+if [ $USEFROGS ] ; then
+    [[ "$5" ]] && FROGSMSCWDIR="$5"
+else
+    [[ "$5" ]] && FROGSMSCWDIR="EMPTY-set_in_ANALYSIS.evndisp.sh"
+fi
+
 # Read runlist
-if [[ ! -f "$RLIST" ]]; then
+if [ ! -f "$RLIST" ] ; then
     echo "Error, runlist $RLIST not found, exiting..."
     exit 1
 fi
-RUNNUMS=`cat $RLIST`
+FILES=`cat $RLIST`
 
-# Log file directory
+# Output directory for error/output
 DATE=`date +"%y%m%d"`
 LOGDIR="$VERITAS_USER_LOG_DIR/$DATE/EVNDISP.ANADATA"
-echo -e "Log files will be written to:\n $LOGDIR"
 mkdir -p $LOGDIR
-
-# output directory
-echo -e "Output files will be written to:\n $ODIR"
-mkdir -p $ODIR
 
 # Job submission script
 SUBSCRIPT="$EVNDISPSYS/scripts/VTS/helper_scripts/ANALYSIS.evndisp_sub"
 
 #########################################
 # loop over all files in files loop
-for RUN in $RUNNUMS; do
-    echo "Now starting run $RUN"
-    FSCRIPT="$LOGDIR/EVN.data-$RUN"
+for AFILE in $FILES
+do
+    echo "Now starting run $AFILE"
+    FSCRIPT="$LOGDIR/EVN.data-$AFILE"
 
-    sed -e "s|RUNFILE|$RUN|"              \
+    sed -e "s|RUNFILE|$AFILE|"              \
         -e "s|CALIBRATIONOPTION|$CALIB|"    \
         -e "s|OUTPUTDIRECTORY|$ODIR|"       \
+        -e "s|MSCWDIRECTORY|$FROGSMSCWDIR|" \
         -e "s|USEVPMPOINTING|$VPM|" $SUBSCRIPT.sh > $FSCRIPT.sh
 
     chmod u+x $FSCRIPT.sh
@@ -81,7 +93,6 @@ for RUN in $RUNNUMS; do
     SUBC=`eval "echo \"$SUBC\""`
     if [[ $SUBC == *qsub* ]]; then
         JOBID=`$SUBC $FSCRIPT.sh`
-        
         # account for -terse changing the job number format
         if [[ $SUBC != *-terse* ]] ; then
             echo "without -terse!"      # need to match VVVVVVVV  8539483  and 3843483.1-4:2
@@ -94,7 +105,6 @@ for RUN in $RUNNUMS; do
             echo "RUN $RUN OLOG $FSCRIPT.sh.o$JOBID"
             echo "RUN $RUN ELOG $FSCRIPT.sh.e$JOBID"
         fi
-        
     elif [[ $SUBC == *parallel* ]]; then
         echo "$FSCRIPT.sh &> $FSCRIPT.log" >> $LOGDIR/runscripts.dat
         echo "RUN $RUN OLOG $FSCRIPT.log"
