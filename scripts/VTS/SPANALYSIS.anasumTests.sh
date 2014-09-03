ec18156153dd5f076d914ff0e33c404767cb613d
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2014-09-03 07:45:47 +0000

    testing script changes (needs to be modified by any user)
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/trunk@2696 766874ec-0625-4018-90f7-740f284a29e6

--- scripts/VTS/SPANALYSIS.anasumTests.sh
+++ scripts/VTS/SPANALYSIS.anasumTests.sh
@@ -7,49 +7,65 @@
 #
 
 # evndisp version
-VERSION="v447"
+VERSION="v451"
 # evndisp analysis date
-DATE="d20140715"
+DATE="d20140801"
+# run list
+LIST="runlist_releaseTesting"
+# evndisp run parameter files
+ERUN="EVNDISP.reconstruction.runparameter.DISP"
+ERUN="EVNDISP.reconstruction.runparameter"
+# reconstruction methods (should match IDs)
+RECMETHODS=( "GEO" "DISP" )
 
 # V4
 SIMTYPE="GRISU"
 EPOCH="V4"
-LIST="/lustre/fs5/group/cta/users/maierg/VERITAS/analysis/Results/Crab/d20140624/Crab_V4.dat"
-
-
-# V5
-SIMTYPE="GRISU"
-EPOCH="V5"
-LIST="/lustre/fs5/group/cta/users/maierg/VERITAS/analysis/Results/Crab/d20140624/Crab_V5_Winter_20112012_gt65deg.dat"
 
 # V6
 SIMTYPE="CARE_June1425"
 EPOCH="V6"
-LIST="/lustre/fs5/group/cta/users/maierg/VERITAS/analysis/Results/Crab/d20140606//Crab_V6_gt65deg.dat"
-LIST="runlist_releaseTesting"
 
-# for C in moderate2tel moderate3tel hard2tel hard3tel superhard open
-# for C in soft softExt moderateExt2tel hard2Exttel
+# V5
+SIMTYPE="GRISU-SW6"
+EPOCH="V5"
+
+# PKS1424p240: V4, V5, V6
+# M82: V4, V5, V6
+# Tycho: V6
 
 # object
 # for O in Crab M82 PKS1424p240 Tycho
-for O in Tycho PKS1424p240 M82
+# for O in Tycho PKS1424p240 M82
+# for O in Tycho PKS1424p240 W44 SS433
+# for O in M82 PKS1424p240 Crab W44 SS433
+for O in Tycho
 do
-    # cut
-#    for C in moderate2tel
-    for C in moderate2tel superhard hard2tel soft moderateopen softopen hardopen moderate3tel hard3tel softExt moderateExt2tel hard2Exttel
+    RLIST="$VERITAS_USER_DATA_DIR/analysis/Results/${VERSION}/$O/${DATE}/${LIST}${EPOCH}.dat"
+    ODIR="$VERITAS_USER_DATA_DIR/analysis/Results/${VERSION}/$O/${DATE}/${VERSION}"
+    mkdir -p $ODIR
+    ########### START EVNDISP
+    ./ANALYSIS.evndisp.sh $RLIST $ODIR $ERUN
+    continue
+    ########### END EVNDISP
+    # reconstruction ID
+    for ID in 0
     do
-        # reconstruction ID
-        for ID in 0 1
+        IDIR="$VERITAS_USER_DATA_DIR/analysis/Results/$VERSION/${O}/${DATE}/${D}/${VERSION}/RecID${ID}_${SIMTYPE}"
+        RM=${RECMETHODS[$ID]}
+        ########### START MSCW
+        ./ANALYSIS.mscw_energy.sh table-$VERSION-auxv01-${SIMTYPE}-ATM21-${EPOCH}-${RM} $RLIST $ODIR $ID $IDIR
+#        continue
+        ########### END MSCW
+        # cut
+#        for C in moderate2tel moderate3tel hard2tel soft 
+        for C in moderate2tel 
         do 
-           for D in v447
-           do
-              for BC in RE RB
+#              for BC in RE RB
+              for BC in RE
               do
-                    IDIR="$VERITAS_USER_DATA_DIR/analysis/Results/${D}/$O/${DATE}/${D}/RecID${ID}_${SIMTYPE}"
-                    ODIR="$VERITAS_USER_DATA_DIR/analysis/Results/${D}/$O/${DATE}/${D}_${EPOCH}_anasum/${D}_ID${ID}_${C}_${BC}"
-                    RUNPAR="$VERITAS_USER_DATA_DIR/analysis/Results/${D}/$O/${DATE}/runparameter.dat"
-                    RLIST="$VERITAS_USER_DATA_DIR/analysis/Results/${D}/$O/${DATE}/${LIST}${EPOCH}.dat"
+                    ODIR="$VERITAS_USER_DATA_DIR/analysis/Results/$VERSION/${O}/${DATE}/${VERSION}_${EPOCH}_anasum/${RM}_ID${ID}_${C}_${BC}"
+                    RUNPAR="$VERITAS_USER_DATA_DIR/analysis/Results/$VERSION/${O}/${DATE}/runparameter.dat"
                     
                     echo $IDIR
                     echo $ODIR
@@ -57,11 +73,10 @@ do
                     echo $RLIST
 
                     rm -f $ODIR.log
-               $EVNDISPSYS/bin/anasum -d $ODIR -i 1 -f $RUNPAR -l $ODIR/$C.anasum.dat -o $ODIR.root > $ODIR.log  
-#                   ./ANALYSIS.anasum_parallel_from_runlist.sh ${RLIST} $ODIR $C ${BC} $RUNPAR $IDIR $SIMTYPE $ID
+                     $EVNDISPSYS/bin/anasum -d $ODIR -i 1 -f $RUNPAR -l $ODIR/$C.anasum.dat -o $ODIR.root > $ODIR.log  
+#                   ./ANALYSIS.anasum_parallel_from_runlist.sh ${RLIST} $ODIR $C ${BC} $RUNPAR $IDIR $SIMTYPE $RM 21
                    echo "DONE $ODIR"
                done
-           done
         done
     done
 done
