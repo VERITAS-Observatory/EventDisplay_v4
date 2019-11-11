5386daec008c771094b9f11f70eb3c89a963a1d1
maierg <maierg@766874ec-0625-4018-90f7-740f284a29e6>
2019-11-11 16:49:49 +0000

    proper generation of Eventdisplay version
    git-svn-id: https://svnsrv.desy.de/desy/EVNDISP/branches/v480g-pre@5748 766874ec-0625-4018-90f7-740f284a29e6

--- scripts/VTS/IRF.generate_effective_area_parts.sh
+++ scripts/VTS/IRF.generate_effective_area_parts.sh
@@ -50,6 +50,7 @@ bash $(dirname "$0")"/helper_scripts/UTILITY.script_init.sh"
 
 # EventDisplay version
 EDVERSION=`$EVNDISPSYS/bin/makeEffectiveArea --version | tr -d .| sed -e 's/[a-Z]*$//'`
+EDVERSION=`$EVNDISPSYS/bin/makeEffectiveArea --version | tr -d .`
 
 # Parse command line arguments
 CUTSFILE="$1"
