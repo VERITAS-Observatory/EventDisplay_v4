678966c784393856c8695153b80066fa8c892257
GernotMaier <gernot.maier@desy.de>
2020-03-04 15:21:54 +0100

    Change of version to v481 (from v480)

--- scripts/VTS/renameIRFs.sh
+++ scripts/VTS/renameIRFs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# eventdisplay v480 is a 'pure' BDT release, meaning that the code for the regular analysis did not change.
-# Thus, the content of the (non-BDT) IRFs did not change, but the filename does (v470 -> v480). 
+# eventdisplay v481 is a 'pure' BDT release, meaning that the code for the regular analysis did not change.
+# Thus, the content of the (non-BDT) IRFs did not change, but the filename does (v470 -> v481). 
 # To avoid having to re-upload the IRFs to the archive with new names, we provide this script. 
 # The user is expected to input 2 directories. The first one is expected to contain the v470 IRFs.
 # The second directory will be created if it does not exist. By default, links to the old IRFs will be put in there
@@ -11,8 +11,8 @@
 if [[ $# < 2 ]]; then
 # begin help message
 echo "
-This is a script to link/copy/mv v470 IRFs to confirm to v480 file name standards.
-Usage: $0 <v470_dir> <v480_dir> [command]
+This is a script to link/copy/mv v470 IRFs to confirm to v481 file name standards.
+Usage: $0 <v470_dir> <v481_dir> [command]
 where the IRFs are expected in v470_dir/Tables, v470_dir/EffectiveAreas etc and command is eg. \"ln -s\" or \"cp -v\" .
 Default is ln -sv.
 Old and new directory may be identical.
@@ -31,7 +31,7 @@ do
 	for file in $OLD/$dir/*root 
 	do
 		name=`basename $file`
-		newname=${name/v470/v480}
+		newname=${name/v470/v481}
 		if [[ "$name" != "$newname" ]]
 		then
 			$COM -v $OLD/$dir/$name $NEW/$dir/$newname 
