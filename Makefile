f56d68de7f45b01a960eae591b4c18dfdbc82700
GernotMaier <gernot.maier@desy.de>
2020-03-08 15:01:57 +0100

    Change version number

--- Makefile
+++ Makefile
@@ -37,7 +37,7 @@ ARCH = $(shell uname)
 # basic numbers 
 #############################
 package = EVNDISP
-version = 481
+version = 482
 # version of auxiliary files
 auxversion = $(version)-auxv01
 distdir = $(package)-$(version)
--- src/VGlobalRunParameter.cpp
+++ src/VGlobalRunParameter.cpp
@@ -351,7 +351,7 @@ void VGlobalRunParameter::printGlobalRunParameter()
 string VGlobalRunParameter::fObservatory = "Whipple";
 bool VGlobalRunParameter::bReadRunParameter = false;
 unsigned int VGlobalRunParameter::fEVNDISP_TREE_VERSION = 9;
-string VGlobalRunParameter::fEVNDISP_VERSION = "v.4.81";
+string VGlobalRunParameter::fEVNDISP_VERSION = "v.4.82";
 string VGlobalRunParameter::fEVNDISP_SVNREVISION = "$Revision: 3624$";
 string VGlobalRunParameter::fDBServer = "";
 string VGlobalRunParameter::fRawDataServer = "";
