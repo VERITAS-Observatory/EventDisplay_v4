92676eff03e96bd72abc7e9e410ec687a5dafd89
Gernot Maier <gernot.maier@desy.de>
2024-09-09 16:20:17 +0200

    astyle 3.6

--- macros/VTS/getSens.C
+++ macros/VTS/getSens.C
@@ -4,68 +4,67 @@
 
 void getSens( TString anasumfile, TString outputbasename )
 {
-	fstream file;
-	// Backup streambuffers of  cout
-	streambuf* stream_buffer_cout = cout.rdbuf();
-	streambuf* stream_buffer_cin = cin.rdbuf();
-	// Get the streambuffer of the file
-	streambuf* stream_buffer_file;
-	
-	gSystem->Load( "$EVNDISPSYS/lib/libVAnaSum.so" );
-	VSensitivityCalculator a;
-	TCanvas* c;
-	// signif, minevts,obstime,minbackgrrate,alpha
-	a.setSignificanceParameter( 5, 10, 50, 0.05, 0.2 );
-	//a.plotIntegralSensitivityvsEnergyFromCrabSpectrum(c,
-	//    anasumfile.Data(),1,"CU",0.03,1e5);
-	a.plotIntegralSensitivityvsEnergyFromCrabSpectrum( c,
-			anasumfile.Data(), 1, "CU", 0.15 );
-			
-	TGraphAsymmErrors* tg = ( TGraphAsymmErrors* ) a.getSensitivityGraph();
-	// Redirect output to file
-	file.open( Form( "%s_intsens50.txt", outputbasename.Data() ), ios::out );
-	stream_buffer_file = file.rdbuf();
-	// Redirect cout to file
-	cout.rdbuf( stream_buffer_file );
-	cout << "# Sensitivity (int) VTS from Crab analysis (in CU)" << endl;
-	cout << "# X, EXlow, EXhigh, Y, EYlow, EYhigh" << endl;
-	for( Int_t k = 0; k < tg->GetN(); k++ )
-	{
-		cout << Form( "%.3e, %.3e, %.3e, %.3e, %.3e, %.2e",
-					  tg->GetX()[k],
-					  tg->GetEXlow()[k],
-					  tg->GetEXhigh()[k],
-					  tg->GetY()[k],
-					  tg->GetEYlow()[k],
-					  tg->GetEYhigh()[k] ) << endl;
-	}
-	// Recover standard output
-	cout.rdbuf( stream_buffer_cout );
-	file.close();
-	
-	a.plotDifferentialSensitivityvsEnergyFromCrabSpectrum( c,
-			anasumfile.Data(), 1, "CU", 0.15 );
-			
-	TGraphAsymmErrors* tg = ( TGraphAsymmErrors* ) a.getSensitivityGraph();
-	// Redirect output to file
-	file.open( Form( "%s_diffsens50.txt", outputbasename.Data() ), ios::out );
-	stream_buffer_file = file.rdbuf();
-	// Redirect cout to file
-	cout.rdbuf( stream_buffer_file );
-	cout << "# Sensitivity (diff) VTS from Crab analysis (in CU)" << endl;
-	cout << "# X, EXlow, EXhigh, Y, EYlow, EYhigh" << endl;
-	for( Int_t k = 0; k < tg->GetN(); k++ )
-	{
-		cout << Form( "%.3e, %.3e, %.3e, %.3e, %.3e, %.2e",
-					  tg->GetX()[k],
-					  tg->GetEXlow()[k],
-					  tg->GetEXhigh()[k],
-					  tg->GetY()[k],
-					  tg->GetEYlow()[k],
-					  tg->GetEYhigh()[k] ) << endl;
-	}
-	// Recover standard output
-	cout.rdbuf( stream_buffer_cout );
-	file.close();
-}
+    fstream file;
+    // Backup streambuffers of  cout
+    streambuf* stream_buffer_cout = cout.rdbuf();
+    streambuf* stream_buffer_cin = cin.rdbuf();
+    // Get the streambuffer of the file
+    streambuf* stream_buffer_file;
 
+    gSystem->Load( "$EVNDISPSYS/lib/libVAnaSum.so" );
+    VSensitivityCalculator a;
+    TCanvas* c;
+    // signif, minevts,obstime,minbackgrrate,alpha
+    a.setSignificanceParameter( 5, 10, 50, 0.05, 0.2 );
+    //a.plotIntegralSensitivityvsEnergyFromCrabSpectrum(c,
+    //    anasumfile.Data(),1,"CU",0.03,1e5);
+    a.plotIntegralSensitivityvsEnergyFromCrabSpectrum( c,
+            anasumfile.Data(), 1, "CU", 0.15 );
+
+    TGraphAsymmErrors* tg = ( TGraphAsymmErrors* ) a.getSensitivityGraph();
+    // Redirect output to file
+    file.open( Form( "%s_intsens50.txt", outputbasename.Data() ), ios::out );
+    stream_buffer_file = file.rdbuf();
+    // Redirect cout to file
+    cout.rdbuf( stream_buffer_file );
+    cout << "# Sensitivity (int) VTS from Crab analysis (in CU)" << endl;
+    cout << "# X, EXlow, EXhigh, Y, EYlow, EYhigh" << endl;
+    for( Int_t k = 0; k < tg->GetN(); k++ )
+    {
+        cout << Form( "%.3e, %.3e, %.3e, %.3e, %.3e, %.2e",
+                      tg->GetX()[k],
+                      tg->GetEXlow()[k],
+                      tg->GetEXhigh()[k],
+                      tg->GetY()[k],
+                      tg->GetEYlow()[k],
+                      tg->GetEYhigh()[k] ) << endl;
+    }
+    // Recover standard output
+    cout.rdbuf( stream_buffer_cout );
+    file.close();
+
+    a.plotDifferentialSensitivityvsEnergyFromCrabSpectrum( c,
+            anasumfile.Data(), 1, "CU", 0.15 );
+
+    TGraphAsymmErrors* tg = ( TGraphAsymmErrors* ) a.getSensitivityGraph();
+    // Redirect output to file
+    file.open( Form( "%s_diffsens50.txt", outputbasename.Data() ), ios::out );
+    stream_buffer_file = file.rdbuf();
+    // Redirect cout to file
+    cout.rdbuf( stream_buffer_file );
+    cout << "# Sensitivity (diff) VTS from Crab analysis (in CU)" << endl;
+    cout << "# X, EXlow, EXhigh, Y, EYlow, EYhigh" << endl;
+    for( Int_t k = 0; k < tg->GetN(); k++ )
+    {
+        cout << Form( "%.3e, %.3e, %.3e, %.3e, %.3e, %.2e",
+                      tg->GetX()[k],
+                      tg->GetEXlow()[k],
+                      tg->GetEXhigh()[k],
+                      tg->GetY()[k],
+                      tg->GetEYlow()[k],
+                      tg->GetEYhigh()[k] ) << endl;
+    }
+    // Recover standard output
+    cout.rdbuf( stream_buffer_cout );
+    file.close();
+}
--- macros/VTS/plot_tmva_rejBvsS.C
+++ macros/VTS/plot_tmva_rejBvsS.C
@@ -4,7 +4,7 @@
  *
  * input are the complete BDT output files
  *
- * usage e.g, 
+ * usage e.g,
  * root -l -q -b 'plot_tmva_rejBvsS.C("V6_2012_2013a_ATM61/NTel2-Moderate/complete_BDTroot/", "V6_2021_2022w_ATM61/NTel2-Moderate/complete_BDTroot")'
  *
  *
@@ -14,28 +14,28 @@ TH1D* get_histogram( string iFile, int iColor )
 {
     TFile *f = new TFile( iFile.c_str() );
     if( f->IsZombie() ) return 0;
-    f->cd("Method_BDT/BDT_0");
-    TH1D *MVA_BDT_0_rejBvsS = (TH1D*)gDirectory->Get( "MVA_BDT_0_rejBvsS" );
-    if( !MVA_BDT_0_rejBvsS) return 0;
+    f->cd( "Method_BDT/BDT_0" );
+    TH1D *MVA_BDT_0_rejBvsS = ( TH1D* )gDirectory->Get( "MVA_BDT_0_rejBvsS" );
+    if(!MVA_BDT_0_rejBvsS ) return 0;
 
     MVA_BDT_0_rejBvsS->SetTitle( "" );
     MVA_BDT_0_rejBvsS->SetStats( 0 );
-    MVA_BDT_0_rejBvsS->SetLineWidth(2);
-    MVA_BDT_0_rejBvsS->SetLineColor(iColor);
+    MVA_BDT_0_rejBvsS->SetLineWidth( 2 );
+    MVA_BDT_0_rejBvsS->SetLineColor( iColor );
     MVA_BDT_0_rejBvsS->SetAxisRange( 0.45, 1. );
 
     return MVA_BDT_0_rejBvsS;
 }
 
 /*
- * expect files names to be e.g. BDT_2_1.root 
+ * expect files names to be e.g. BDT_2_1.root
  *
  */
-void plot_tmva_rejBvsS( 
-        string iFileDir1,
-        string iFileDir2,
-        unsigned int ebin_min, unsigned int ebin_max,
-        unsigned int zebin_min, unsigned int zebin_max )
+void plot_tmva_rejBvsS(
+    string iFileDir1,
+    string iFileDir2,
+    unsigned int ebin_min, unsigned int ebin_max,
+    unsigned int zebin_min, unsigned int zebin_max )
 {
     TCanvas *c = new TCanvas( "c", "", 100, 10, 400, 400 );
     c->SetLeftMargin( 0.12 );
@@ -50,8 +50,8 @@ void plot_tmva_rejBvsS(
             ostringstream iFile2;
             iFile2 << iFileDir2 << "/BDT_" << e << "_" << z << ".root";
 
-            TH1D *h1 = get_histogram(iFile1.str(), 1 );
-            TH1D *h2 = get_histogram(iFile2.str(), 2 );
+            TH1D *h1 = get_histogram( iFile1.str(), 1 );
+            TH1D *h2 = get_histogram( iFile2.str(), 2 );
 
             if( h1 )
             {
@@ -66,4 +66,4 @@ void plot_tmva_rejBvsS(
             c->Print( printName.str().c_str() );
         }
     }
- }
+}
