331b7de237377cae4b6aea0f76ab1023e2bdaff2
GernotMaier <gernot.maier@desy.de>
2025-11-29 12:29:05 +0100

    add 2D MLP

--- src/applyTMVAforDirection.cpp
+++ src/applyTMVAforDirection.cpp
@@ -27,10 +27,11 @@ static const vector< string > training_variables = {
 void evaluate( string iInputFile, string iTMVAWeightsDir, string iOutputFile )
 {
     const unsigned int n_tel_max = 4;
+    bool is_MLP = true;
 
     TFile outputFile( iOutputFile.c_str(), "RECREATE" );
     TTree* outTree = new TTree(
-        "DispDirection", "Direction reconstruction results based on BDT regression");
+        "DispDirection", "Direction reconstruction results based on MVA regression");
     float dir_Xoff;
     float dir_Yoff;
     outTree->Branch("Dir_Xoff", &dir_Xoff, "Dir_Xoff/F");
@@ -87,14 +88,23 @@ void evaluate( string iInputFile, string iTMVAWeightsDir, string iOutputFile )
 
     // - first dimension: X, Y, second dimension: n_tel = 2,3,4
     vector< vector< TMVA::Reader* > > fTMVAReader;
-    for( unsigned int xy = 0; xy < 2; xy++ )
+    unsigned int n_xy = 2;
+    if( is_MLP ) n_xy = 1;
+    for( unsigned int xy = 0; xy < n_xy; xy++ )
     {
         string coord = ( xy == 0 ) ? "X" : "Y";
         vector< TMVA::Reader* > iTMVAReader;
         for( unsigned int i = 2; i <= n_tel_max; i++ )
         {
             ostringstream weightFileName;
-            weightFileName << iTMVAWeightsDir << "/DISPDir" << coord << "off_ntel" << i << "_BDT_" << coord << "off.weights.xml";
+            if( is_MLP )
+            {
+                weightFileName << iTMVAWeightsDir << "/DISPDirMLP_ntel" << i << "_MLP_MultiTarget.weights.xml";
+            }
+            else
+            {
+                weightFileName << iTMVAWeightsDir << "/DISPDir" << coord << "off_ntel" << i << "_BDT_" << coord << "off.weights.xml";
+            }
             cout << "  weight file " << weightFileName.str() << endl;
             TMVA::Reader* reader = new TMVA::Reader("!Color:!Silent");
             // Add variables to reader
@@ -107,36 +117,51 @@ void evaluate( string iInputFile, string iTMVAWeightsDir, string iOutputFile )
                     reader->AddVariable(var.str().c_str(), &mva_arrBuf[v][n]);
                 }
             }
-            if( xy == 0 )
+            if( is_MLP )
             {
                 for( unsigned int n = 0; n < i; n++ )
                 {
                     ostringstream var;
                     var << "disp_x_" << n;
                     reader->AddVariable(var.str().c_str(), &mva_disp_x[n]);
-                }
-            }
-            else
-            {
-                for( unsigned int n = 0; n < i; n++ )
-                {
-                    ostringstream var;
+                    var.str("");
                     var << "disp_y_" << n;
                     reader->AddVariable(var.str().c_str(), &mva_disp_y[n]);
                 }
             }
+            else
+            {
+                if( xy == 0 )
+                {
+                    for( unsigned int n = 0; n < i; n++ )
+                    {
+                        ostringstream var;
+                        var << "disp_x_" << n;
+                        reader->AddVariable(var.str().c_str(), &mva_disp_x[n]);
+                    }
+                }
+                else
+                {
+                    for( unsigned int n = 0; n < i; n++ )
+                    {
+                        ostringstream var;
+                        var << "disp_y_" << n;
+                        reader->AddVariable(var.str().c_str(), &mva_disp_y[n]);
+                    }
+                }
+            }
             reader->AddVariable("Xoff_weighted_bdt", &mva_Xoff_weighted_bdt);
             reader->AddVariable("Yoff_weighted_bdt", &mva_Yoff_weighted_bdt);
-            if( xy == 0 )
+            if( xy == 0 || is_MLP )
             {
                 reader->AddVariable("Xoff_intersect", &mva_Xoff_intersect);
             }
-            else
+            if( xy == 1 || is_MLP )
             {
                 reader->AddVariable("Yoff_intersect", &mva_Yoff_intersect);
             }
             reader->AddSpectator("MCe0", &tmp_var);
-            if( !reader->BookMVA("BDT", weightFileName.str().c_str()) )
+            if( !reader->BookMVA("MVA", weightFileName.str().c_str()) )
             {
                 cout << "Error: cannot find TMVA weight file: " << weightFileName.str() << endl;
                 exit( EXIT_FAILURE );
@@ -179,8 +204,16 @@ void evaluate( string iInputFile, string iTMVAWeightsDir, string iOutputFile )
             mva_disp_x[i] = arrBuf[0][i] * arrBuf[3][n_index] + fpointing_dx[n_index]; // Disp_T * cosphi
             mva_disp_y[i] = arrBuf[0][i] * arrBuf[4][n_index] + fpointing_dy[n_index]; // Disp_T * sinphi
         }
-        dir_Xoff = fTMVAReader[0][DispNImages-2]->EvaluateMVA("BDT");
-        dir_Yoff = fTMVAReader[1][DispNImages-2]->EvaluateMVA("BDT");
+        if( is_MLP )
+        {
+            dir_Xoff = fTMVAReader[0][DispNImages-2]->EvaluateRegression("MVA")[0];
+            dir_Yoff = fTMVAReader[0][DispNImages-2]->EvaluateRegression("MVA")[1];
+        }
+        else
+        {
+            dir_Xoff = fTMVAReader[0][DispNImages-2]->EvaluateMVA("MVA");
+            dir_Yoff = fTMVAReader[1][DispNImages-2]->EvaluateMVA("MVA");
+        }
 
         outTree->Fill();
     }
