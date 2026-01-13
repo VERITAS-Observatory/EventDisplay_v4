eaf50349b4ec55cbd7d97d639344619e2052c58e
GernotMaier <gernot.maier@desy.de>
2026-01-13 18:05:38 +0100

    absolute essential important bug fix when getting zenith angle for MC files

--- src/printRunParameter.cpp
+++ src/printRunParameter.cpp
@@ -103,7 +103,8 @@ bool readRunParameter( TFile* fIn, string iPara )
                                           iPara.find( "updated-runinfo" ) != string::npos ) << "\t";
         cout << fPar->getInstrumentEpoch( true ) << "\t";
         cout << fPar->getAtmosphereID( iPara.find( "updated-runinfo" ) != string::npos ) << "\t";
-        cout << fPar->fDBRunType << "\t";
+        if (fPar->fDBRunType.size() > 0 ) cout << fPar->fDBRunType << "\t";
+        else cout << "no_db_runtype \t";
         for( unsigned int i = 0; i < fPar->fTelToAnalyze.size(); i++ )
         {
             cout << fPar->fTelToAnalyze[i] + 1;
