7b23c7d683f6a3ab9e1b60629e780712028fb2a1
Gernot Maier <gernot.maier@desy.de>
2026-05-29 09:06:10 +0200

    A13

--- src/trainTMVAforGammaHadronSeparation.cpp
+++ src/trainTMVAforGammaHadronSeparation.cpp
@@ -257,7 +257,7 @@ bool train( VTMVARunData* iRun,
         cout << "error during training: energy bin out of range " << iEnergyBin << endl;
         return false;
     }
-    if( iRun->fZenithCutData.size() < iZenithBin || iRun->fOutputFile[0].size() < iZenithBin )
+    if( iRun->fZenithCutData.size() <= iZenithBin || iRun->fOutputFile[0].size() <= iZenithBin )
     {
         cout << "error during training: zenith bin out of range " << iZenithBin << endl;
         return false;
