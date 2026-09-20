ce916049e7aa8bcef974fd895d32a5ba6774bd0a
GernotMaier <gernot.maier@desy.de>
2026-09-20 15:25:23 +0200

    build

--- install_sofa.sh
+++ install_sofa.sh
@@ -27,15 +27,18 @@ mkdir -p sofa
 cd sofa
 
 # get sofa package from the web page and install
-SOFAD="20210512"
+SOFAD="20231011"
 SOFA="sofa_c-${SOFAD}.tar.gz"
 if [[ -e sofa.tar.gz ]]; then
     mv -f sofa.tar.gz ${SOFA}
 elif [[ $DOWNL == "CI" ]]; then
-    wget https://syncandshare.desy.de/index.php/s/jr9NrbWFR5MLaDf/download
-    mv -f download ${SOFA}
+    wget --no-check-certificate \
+        https://www.iausofa.org/s/sofa_c-20231011tar.gz \
+        -O ${SOFA}
 else
-    wget --no-check-certificate https://www.iausofa.org/2021_0512_C/${SOFA}
+    wget --no-check-certificate \
+        https://www.iausofa.org/s/sofa_c-20231011tar.gz \
+        -O ${SOFA}
 fi
 if [ ! -e ${SOFA} ]
 then
